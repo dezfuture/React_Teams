48ba68194d09f8cdcba7068bed45b2363254c7de
dezfuture <agrim2000@gmail.com>
2021-08-03 01:51:59 +0530

    initialized the chatEngine.io setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { ChatEngine } from "react-chat-engine";
+
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>
-        <strong>hey there!!</strong>
-      </h1>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="6684c04f-464e-488e-86b8-8250f3413fef"
+      userName="Shawtyyy"
+      userSecret="jatin"
+    />
   );
 };
 
